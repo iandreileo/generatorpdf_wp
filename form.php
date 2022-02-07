b71716d3717d53001bac15662b2b7f14528f1d10
Ilie Andrei-Leonard <44784039+sKzRO@users.noreply.github.com>
2022-02-07 14:17:48 +0200

    Create form.php

---
+++ form.php
@@ -0,0 +1,52 @@
+<label> Nume
+    [text* nume] </label>
+
+<label> Prenume
+    [text* prenume] </label>
+
+<label> Initiala tatalui
+    [text* initialaTatalui] </label>
+
+<label> CNP
+    [text* cnp] </label>
+
+<label> Email
+    [email* email] </label>
+
+<label> Telefon
+    [text* telefon] </label>
+
+Adresa de domiciliu
+
+<label> Strada
+    [text* strada] </label>
+
+<label> Numar
+    [text* numar] </label>
+
+<label> Bloc
+    [text* bloc] </label>
+
+<label> Scara
+    [text* scara] </label>
+
+<label> Etaj
+    [text* et] </label>
+
+<label> Apartament
+    [text* ap] </label>
+
+<label> Judet/Sector
+    [text* judet] </label>
+
+<label> Localitate
+    [text* localitate] </label>
+
+<label> Cod postal
+    [text* codPostal] </label>
+
+[acceptance acceptance-990] Sunt de acord cu prelucrarea datelor cu caracter personal. [/acceptance]
+
+[hidden test]
+
+[submit "Trimite"]
